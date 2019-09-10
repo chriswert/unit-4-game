169cf3356f2bf1f6979e526553c47fb2e2ad7568
Christopher Werther <wildcatwerther@gmail.com>
2019-09-10 16:46:06 -0700

    first part of JQuery code

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    //Global Var
+
+var randomNumber;
+var winLosses;
+var kirk;
+var picard;
+var sisko;
+var janeway;
+var score;
+var totalScore;
+
+w
+
+
+
+
+
+
+
+
+
+})
