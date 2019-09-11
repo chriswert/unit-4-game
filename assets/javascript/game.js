737d3f6780bc3f05f23a47337f5af6d81b2445fc
Christopher Werther <wildcatwerther@gmail.com>
2019-09-11 16:24:21 -0700

    randomNumber javascript code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    //Global Var
+//Global Var
 
 var randomNumber;
 var winLosses;
@@ -11,12 +11,59 @@ var janeway;
 var score;
 var totalScore;
 
-w
+// write function to select a random number between 20 - 135.
+
+function randomNumber() {
+   return Math.floor(Mth.random() * 115) + 20;   
+}
+console.log();
+
+// display random number on web page
+
+
+
+
+// write function to select a random number for each button
+
+function randomNumCaptains() {
+    return {
+        kirk: {
+            points: Math.floor(Math.random() * 15) + 1,
+            imageUrl: "assets/images/kirk.jpg"
+        },
+        picard: {
+            points: Math.floor(Math.random() * 15) + 1,
+            imageUrl: "assets/images/picard.jpg"
+        },
+        sisko: {
+            points: Math.floor(Math.random() * 15) + 1,
+            imageUrl: "assets/images/sisko.jpg"
+        },
+        janeway: {
+            points: Math.floor(Math.random() * 15) + 1,
+            imageUrl: "assets/images/janeway.jpg"
+        }
+    };
+    console.log();
+}
+
+// reset random number after win or loss
+
+
+
+
+// write function to display the total score for buttons selected
 
 
 
 
 
+// display wins or losses
+
+
+
+
+// end game and reset
 
 
 
