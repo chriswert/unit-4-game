353fe9e7ba780b8e1658a504cbf7916e288ab405
Christopher Werther <wildcatwerther@gmail.com>
2019-09-12 20:14:38 -0700

    button random number code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,19 +3,24 @@ $(document).ready(function(){
 //Global Var
 
 var randomNumber;
-var win;
-var Losses;
-var score;
+var wins = 0;
+var Losses = 0;
+var userScore = 0;
+var game;
+
+var num1 = Math.floor(Math.random() * 12 + 1)
+var num2 = Math.floor(Math.random() * 12 + 1)
+var num3 = Math.floor(Math.random() * 12 + 1)
+var num4 = Math.floor(Math.random() * 12 + 1)
 
 
 
 
-// write function to select a random number between 20 - 135.
+// write function to select a random number between 20 - 99.
 
 function randomNumber() {
-   return Math.floor(Math.random() * 115) + 20;   
+   return Math.floor(Math.random() * 99) + 20;   
 }
-
 let num = randomNumber();
 console.log(num); 
 
@@ -27,17 +32,78 @@ $('.randomNumGen').text(num);
 
 // write function to select a random number for each button
 
-for (var i = 0; i < 4; i++){
-    var random = Math.floor(Math.random() * 18) + 1;
-    console.log(random)
-
-    var captainButton = $(this);
-    captainButton.attr({
-        "data-random": random
-    })
+function game(){       
+    num1 = Math.floor(Math.random() * 12 + 1);
+    num2 = Math.floor(Math.random() * 12 + 1);
+    num3 = Math.floor(Math.random() * 12 + 1);
+    num4 = Math.floor(Math.random() * 12 + 1);
+    userScore = 0;   
 }
+let start = game()
+console.log(num1, num2, num3, num4);
+$('.totalScore').text(userScore);
 
-//function beginGame() {
+
+$('captain0').on('click', function(){
+    userScore = userScore + num1;
+    
+    $('totalScore').text(userScore);
+    console.log("userScore = " + userScore);
+
+    if(userScore === random){
+        win();
+    }
+    else if (userScore > random){
+        lose()
+    }    
+})
+
+$('captain1').on('click', function(){
+    userScore = userScore + num1;
+    
+    $('totalScore').text(userScore);
+    console.log("userScore = " + userScore);
+
+    if(userScore === random){
+        win();
+    }
+    else if (userScore > random){
+        lose()
+    }    
+})
+
+$('captain2').on('click', function(){
+    userScore = userScore + num1;
+    
+    $('totalScore').text(userScore);
+    console.log("userScore = " + userScore);
+
+    if(userScore === random){
+        win();
+    }
+    else if (userScore > random){
+        lose()
+    }    
+})
+
+$('captain3').on('click', function(){
+    userScore = userScore + num1;
+    
+    $('totalScore').text(userScore);
+    console.log("userScore = " + userScore);
+
+    if(userScore === random){
+        win();
+    }
+    else if (userScore > random){
+        lose()
+    }  
+})
+
+        
+
+    
+
    
 
 
@@ -47,12 +113,22 @@ for (var i = 0; i < 4; i++){
 
 
 // write function to display the total score for buttons selected
-
+   
 
 
 
 
 // display wins or losses
+   function win(){
+       wins++;
+       $('#win').text(wins);
+       reset()
+   }
+   function lose() {
+       losses++;
+       $('#loss').text(losses);
+       reset()
+   }
 
 
 
